d41b43e9d99e94d873585cf01e029778eb085e93
Om29001 <86549121+Om29001@users.noreply.github.com>
2023-03-28 11:56:02 +0530

    server change

--- src/Mycomp/Portfoliopage.jsx
+++ src/Mycomp/Portfoliopage.jsx
@@ -9,7 +9,7 @@ export default function Portfoliopage() {
 
   const callPortfoliopage = async (e) => {
     try {
-      const res = await fetch("https://port-folio-server-omega.vercel.app/portfolio", {
+      const res = await fetch("https://port-folio-server-alpha.vercel.app/portfolio", {
         method: "GET",
         headers: { "Content-Type": "application/json" },
       })
